f8fb0e72364cc824cd9aabe13cc8d38a4d075550
Raphael Pizzo <raphael.pizzo@pizzosoft.com.br>
2022-12-27 13:42:52 -0300

    refactoring de deletar e atualizar com checkbox
    refactoring de deletar e atualizar com checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import style from './App.module.scss';
 import Card from './components/Card';
 import Formulario from './components/Formulario';
@@ -14,20 +14,10 @@ function App() {
 
   const adicionarEvento = (evento: IEvento) => {
     evento.id = Math.round((new Date()).getTime() / 1000)
-   // eventos.push(evento)
+    // eventos.push(evento)
     //console.log(eventos);
 
-//    setEventos([...eventos])
-  }
-  const alterarStatusEvento = (id: number) => {
-  //  const evento = eventos.find(evento => evento.id === id)
-   // if (evento) {
-     // evento.completo = !evento.completo
-   // }
-    //setEventos([...eventos])
-  }
-  const deletarEvento = (id: number) => {
-   // setEventos([...eventos.filter(evento => evento.id !== id)])
+    //    setEventos([...eventos])
   }
 
   const aplicarFiltro = (data: Date | null) => {
@@ -35,25 +25,25 @@ function App() {
   }
 
   //const filtrados = !filtro
-//    ? eventos
- //   : eventos.filter((evento) =>
-   ///   filtro!.toISOString().slice(0, 10) === evento.inicio.toISOString().slice(0, 10)
-    //);
+  //    ? eventos
+  //   : eventos.filter((evento) =>
+  ///   filtro!.toISOString().slice(0, 10) === evento.inicio.toISOString().slice(0, 10)
+  //);
 
   return (
     <RecoilRoot>
       <div className={style.App}>
         <div className={style.Coluna}>
           <Card>
-            <Formulario  />
+            <Formulario />
           </Card>
           <hr />
           <Card>
-            <ListaDeEventos aoFiltroAplicado={aplicarFiltro} aoAlterarStatus={alterarStatusEvento} aoDeletarEvento={deletarEvento} />
+            <ListaDeEventos aoFiltroAplicado={aplicarFiltro}  />
           </Card>
         </div>
         <div className={style.Coluna}>
-          <Calendario  />
+          <Calendario />
         </div>
       </div>
     </RecoilRoot>
--- src/components/ListaDeEventos/index.tsx
+++ src/components/ListaDeEventos/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { IEvento } from '../../interfaces/IEvento';
 import Evento from '../Evento';
 import Filtro from '../Filtro';
 import style from './ListaDeEventos.module.scss';
@@ -7,10 +6,10 @@ import { useRecoilValue } from 'recoil';
 import { listaDeEventosState } from '../../state/atom';
 
 const ListaDeEventos: React.FC<{
-  aoAlterarStatus: (id: number) => void,
-  aoDeletarEvento: (id: number) => void,
+
+
   aoFiltroAplicado: (data: Date | null) => void
-}> = ({ aoDeletarEvento, aoAlterarStatus, aoFiltroAplicado }) => {
+}> = ({ aoFiltroAplicado }) => {
 
   const eventos = useRecoilValue(listaDeEventosState)
 
@@ -18,7 +17,7 @@ const ListaDeEventos: React.FC<{
     <Filtro aoFiltroAplicado={aoFiltroAplicado} />
     <div className={style.Scroll}>
       {eventos.map(evento => (
-        <Evento aoAlterarStatus={aoAlterarStatus} aoDeletarEvento={aoDeletarEvento} evento={evento} key={evento.id} />
+        <Evento evento={evento} key={evento.id} />
       ))}
     </div>
   </section>)
